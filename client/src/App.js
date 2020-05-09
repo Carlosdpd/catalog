d9920d8bf2fe8bdc2f1d5ab3a752ffb33b343f05
Alejandro <temprano.alejandro@gmail.com>
2020-05-09 17:20:52 +0200

    Test component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './Components/Component' 
 
 class App extends Component {
   render() {
@@ -10,9 +11,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MyComponent></MyComponent>
+        <MyComponent></MyComponent>
+        <MyComponent></MyComponent>
+
       </div>
     );
   }
